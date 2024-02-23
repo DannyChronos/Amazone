82ca56b2355fa2840cce606a490ec6e9c7dcda11
rout <dayanetrunks@gmail.com>
2024-02-23 13:09:27 +0100

    Uno

---
+++ database/migrations/2024_02_23_080616_image_articles.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ImageArticles extends Migration
+{
+    public function up()
+    {
+        Schema::create('image_articles', function (Blueprint $table) {
+            $table->id('id_image');
+            $table->string('nom_image');
+            $table->binary('file');
+            $table->string('id');
+            $table->foreign('id')->references('id')->on('articles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
